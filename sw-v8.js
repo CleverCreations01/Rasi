ee9777f292f9cabf2ed1c71c9089afeea5542739
CleverCreations01 <clippingbusinessvyro@gmail.com>
2026-09-25 21:15:59 +0530

    Add fresh RASI service worker

---
+++ sw-v8.js
@@ -0,0 +1,13 @@
+const CACHE='rasi-v8';
+const ASSETS=['./','./index.html','./manifest.json','./sw.js','./icon-192.png','./icon-512.png'];
+self.addEventListener('install',e=>e.waitUntil(caches.open(CACHE).then(c=>c.addAll(ASSETS)).then(()=>self.skipWaiting())));
+self.addEventListener('activate',e=>e.waitUntil(caches.keys().then(keys=>Promise.all(keys.filter(k=>k!==CACHE).map(k=>caches.delete(k)))).then(()=>self.clients.claim())));
+self.addEventListener('fetch',e=>{
+ if(e.request.method!=='GET')return;
+ const isHtml=e.request.mode==='navigate'||e.request.destination==='document';
+ e.respondWith(
+   isHtml
+   ? fetch(e.request).then(res=>{const copy=res.clone();caches.open(CACHE).then(c=>c.put(e.request,copy));return res}).catch(()=>caches.match(e.request).then(r=>r||caches.match('./index.html')))
+   : caches.match(e.request).then(r=>r||fetch(e.request).then(res=>{const copy=res.clone();caches.open(CACHE).then(c=>c.put(e.request,copy));return res}).catch(()=>caches.match('./index.html')))
+ );
+});
